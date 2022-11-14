f6c1830395a76b6e486afefb89a8112ab60c8500
Diego Yon <diegoyon@outlook.com>
2022-11-14 11:23:47 -0600

    user able to select a category for their expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -65,6 +65,6 @@ class ExpensesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def expense_params
-      params.require(:expense).permit(:name, :amount, :user_id)
+      params.require(:expense).permit(:name, :amount, :user_id, category_ids: [])
     end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :expenses
+
+  validates :name, presence: true, uniqueness: true
 end
