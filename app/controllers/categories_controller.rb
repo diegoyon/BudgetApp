0582c22058ce7d4825ab322c85d441cda3f3f1af
Diego Yon <diegoyon@outlook.com>
2022-11-15 15:44:07 -0600

    style new and edit categories page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -36,7 +36,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
+        format.html { redirect_to categories_url, notice: 'Category was successfully updated.' }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
